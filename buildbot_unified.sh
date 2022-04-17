4d804f490b9c296166c96941b2c270da42fe543d
Vgdn1942 <vgdn1942@gmail.com>
2022-04-17 18:27:38 +0300

    April changes

--- buildbot_unified.sh
+++ buildbot_unified.sh
@@ -48,45 +48,35 @@ START=`date +%s`
 BUILD_DATE="$(date +%Y%m%d)"
 BUILD_OUTPUT=~/GSI_treble_build/build-output
 
-export WITHOUT_CHECK_API=true
 export WITH_SU=false
-export OUT_DIR_COMMON_BASE=~/gsi_out
+export WITH_GAPPS=true
+#export OUT_DIR_COMMON_BASE=~/gsi_out
+export PATH=$PATH:~/bin
 
 prep_build() {
     echo "Preparing local manifests"
-    #repo init -u https://github.com/Vgdn1942/android.git -b lineage-19.0
-    #repo init -u https://github.com/AndyCGYan/android.git -b lineage-19.0
-    #repo init -u https://github.com/LineageOS/android.git -b lineage-19.0
+    #repo init -u https://github.com/Vgdn1942/android.git -b lineage-19.1
+    #repo init -u https://github.com/AndyCGYan/android.git -b lineage-19.1
+    #repo init -u https://github.com/LineageOS/android.git -b lineage-19.1
     mkdir -p .repo/local_manifests
-    cp ./lineage_build_unified/local_manifests_${MODE}/*.xml .repo/local_manifests
-    cp ./lineage_build_unified/bv9500plus/local_manifests/*.xml .repo/local_manifests
+    cp ./lineage_build_unified/local_manifests/*.xml .repo/local_manifests
 
+    rm -rf ./lineage_build_unified/bv9500plus/patches_andy_yan
     mkdir -p ./lineage_build_unified/bv9500plus/patches_andy_yan/frameworks_base
-    cp ./lineage_patches_unified/patches_platform_personal/frameworks_base/0004-Disable-FP-lockouts.patch \
+    cp ./lineage_patches_unified/patches_platform_personal/frameworks_base/0003-Disable-FP-lockouts.patch \
         ./lineage_build_unified/bv9500plus/patches_andy_yan/frameworks_base
-    cp ./lineage_patches_unified/patches_platform_personal/frameworks_base/0009-Keyguard-UI-Fix-status-bar-quick-settings-margins-an.patch \
+    cp ./lineage_patches_unified/patches_platform_personal/frameworks_base/0008-Keyguard-UI-Fix-status-bar-quick-settings-margins-an.patch \
         ./lineage_build_unified/bv9500plus/patches_andy_yan/frameworks_base
-    cp ./lineage_patches_unified/patches_platform_personal/frameworks_base/0016-UI-Remove-privacy-dot-padding.patch \
+    cp ./lineage_patches_unified/patches_platform_personal/frameworks_base/0015-UI-Remove-privacy-dot-padding.patch \
         ./lineage_build_unified/bv9500plus/patches_andy_yan/frameworks_base
-
-    # test monet
-    mkdir -p ./lineage_build_unified/bv9500plus/patches_andy_yan/frameworks_base
-    mkdir -p ./lineage_build_unified/bv9500plus/patches_andy_yan/packages_apps_Trebuchet
-    mkdir -p ./lineage_build_unified/bv9500plus/patches_andy_yan/vendor_lineage
-    cp ./lineage_patches_unified/patches_platform_personal/frameworks_base/0001-Add-LineageMonetAccentOverlay.patch \
-        ./lineage_build_unified/bv9500plus/patches_andy_yan/frameworks_base
-    cp ./lineage_patches_unified/patches_platform_personal/frameworks_base/0010-Revert-monet-Add-support-for-monet-cam16.patch \
-        ./lineage_build_unified/bv9500plus/patches_andy_yan/frameworks_base
-    cp ./lineage_patches_unified/patches_platform_personal/packages_apps_Trebuchet/0001-Revert-Implement-LocalColorExtractor-using-monet-the.patch \
-        ./lineage_build_unified/bv9500plus/patches_andy_yan/packages_apps_Trebuchet
-    cp ./lineage_patches_unified/patches_platform_personal/vendor_lineage/0003-Revert-overlay-Enable-monet.patch \
-        ./lineage_build_unified/bv9500plus/patches_andy_yan/vendor_lineage
 
     rm -f ./lineage_patches_unified/patches_treble/system_core/0001-Revert-init-Add-vendor-specific-initialization-hooks.patch # Back vendor_init
-    rm -f ./lineage_patches_unified/patches_treble_phh/platform_frameworks_base/0019-SystemUI-Use-AVCProfileMain-for-screen-recorder.patch # twelve-ultralegacy-devices
-    rm -f ./lineage_patches_unified/patches_platform/frameworks_base/0007-UI-Revive-navbar-layout-tuning-via-sysui_nav_bar-tun.patch # fix bootloop after add lockscreen
-    rm -rf ./packages/overlays/Lineage/customizations/NavigationBarNoHint # fix bootloop after add lockscreen
-    rm -rf ./lineage_patches_unified/patches_treble_phh/platform_system_core/0002-first-stage-If-Vboot2-fails-fall-back-to-Vboot1.patch
+    rm -f ./lineage_patches_unified/patches_platform/frameworks_base/0009-UI-Revive-navbar-layout-tuning-via-sysui_nav_bar-tun.patch # fix bootloop after add lockscreen
+
+    # unneeded patches
+    rm -f ./lineage_patches_unified/patches_platform/vendor_partner_gms/0001-SearchLauncher-Adapt-to-Trebuchet.patch
+    rm -f ./lineage_patches_unified/patches_platform/vendor_partner_gms/0002-SearchLauncher-Fix-build-on-Sv2.patch
+    rm -f ./lineage_patches_unified/patches_platform/frameworks_base/0011-UI-Unblock-alarm-status-bar-icon.patch
 
     rm -rf ./device/phh/treble/miravision
     echo ""
@@ -100,43 +90,16 @@ prep_build() {
     mkdir -p $BUILD_OUTPUT
     echo ""
 
-    repopick -t twelve-monet
-    repopick -Q "status:open+project:LineageOS/android_packages_apps_AudioFX+branch:lineage-19.0"
-    repopick -Q "status:open+project:LineageOS/android_packages_apps_Etar+branch:lineage-19.0"
-    repopick -Q "status:open+project:LineageOS/android_packages_apps_Trebuchet+branch:lineage-19.0+NOT+317783+NOT+318747"
-    repopick -Q "status:open+project:LineageOS/android_packages_apps_Messaging+branch:lineage-19.0"
-    repopick -Q "status:open+project:LineageOS/android_packages_apps_Dialer+branch:lineage-19.0"
-    repopick -Q "status:open+project:LineageOS/android_packages_apps_DeskClock+branch:lineage-19.0"
-    #repopick -Q "status:open+project:LineageOS/android_packages_apps_Recorder+branch:lineage-19.0"
+    repopick -Q "status:open+project:LineageOS/android_packages_apps_Trebuchet+branch:lineage-19.1"
     repopick -t twelve-burnin
-    repopick -t twelve-buttons
-    repopick -t twelve-fingerprint
-    repopick -t twelve-volume-panel-location
-    repopick -t twelve-swap-volume-buttons
-    repopick -t twelve-camera-button
-    repopick -t twelve-navbar-runtime-toggle
-    repopick -t twelve-buttons-lights
-    repopick -t twelve-keyboard-lights
-    repopick 321038 # Settings: Add back increasing ring feature (2/2)
-    repopick -t twelve-statusbar-brightness-and-qs-slider
-    repopick -t twelve-powermenu
+
     repopick 321337 # Deprioritize important developer notifications
     repopick 321338 # Allow disabling important developer notifications
     repopick 321339 # Allow disabling USB notifications
-    repopick -t twelve-qs-tiles
-    repopick -t restricted-networking-mode
-    repopick -t twelve-ultralegacy-devices
-    repopick -t twelve-bash
-    repopick 322554 # Fix concurrency issue with BatteryUsageStats
-    repopick 322555 # Include saved battery history chunks into BatteryUsageStats parcel
-    #repopick -t twelve-data-restriction
-    repopick -Q "status:open+topic:twelve-data-restriction+NOT+322482"
-    repopick -f 322478 # Expose getActiveNetworkForUid to system API
+    repopick 326712 # overlay: show all icons in collapsed statusbar
+    repopick 327113 # Keystore 2.0: Add CREATION_DATETIME only for Keymint V1 and higher.
+
     repopick -f 239371 # SystemUI: Switch back to pre P mobile type icon style
-    repopick -t S_asb_2022-01
-    repopick -t twelve-miscaospfix
-    #repopick -t twelve-legacy-camera
-    repopick -Q "status:open+topic:twelve-legacy-camera+NOT+320541+NOT+320542"
 
     cd frameworks/native
     git revert 340882c64b5944a62b122bbb24f95645c5a0c465 --no-edit # Plumb attribution tag to Sensor Service
@@ -148,54 +111,14 @@ prep_build() {
     cd ../..
 }
 
-gms_patches() {
-    NOW=$PWD
-    cd vendor/partner_gms
-    git clean -fdx; git reset --hard
-    for patch in $NOW/lineage_build_unified/bv9500plus/gms_patches/*.patch; do
-        if git apply --check $patch; then
-            git am $patch
-        elif patch -f -p1 --dry-run < $patch > /dev/null; then
-            #This will fail
-            git am $patch || true
-            patch -f -p1 < $patch
-            git add -u
-            git am --continue
-        else
-            echo "Failed applying $patch"
-        fi
-    done
-    cd ../..
-}
-
-overlay_patches() {
-    NOW=$PWD
-    cd vendor/hardware_overlay
-    git clean -fdx; git reset --hard
-    for patch in $NOW/lineage_build_unified/bv9500plus/overlay_patches/*.patch; do
-        if git apply --check $patch; then
-            git am $patch
-        elif patch -f -p1 --dry-run < $patch > /dev/null; then
-            #This will fail
-            git am $patch || true
-            patch -f -p1 < $patch
-            git add -u
-            git am --continue
-        else
-            echo "Failed applying $patch"
-        fi
-    done
-    cd ../..
+apply_patches() {
+    echo "Applying patch group ${1}"
+    bash ./lineage_build_unified/apply_patches.sh ./lineage_patches_unified/${1}
 }
 
 apply_patches_personal() {
-    echo "Applying patch personal ${1}"
-    bash $PWD/../treble_experimentations/apply-patches.sh ./lineage_build_unified/${1}
-}
-
-apply_patches() {
-    echo "Applying patch group ${1}"
-    bash $PWD/../treble_experimentations/apply-patches.sh ./lineage_patches_unified/${1}
+    echo "Applying personal patch group ${1}"
+    bash ./lineage_build_unified/apply_patches.sh ./lineage_build_unified/${1}
 }
 
 prep_device() {
@@ -216,26 +139,26 @@ finalize_treble() {
     cd device/phh/treble
     git clean -fdx
     bash generate.sh lineage
-    rm lineage_treble_a*.mk
+    rm lineage_a*.mk
     cd ../../..
     tar -xf ./lineage_build_unified/bv9500plus/miravision.tar.gz -C ./device/phh/treble/
 }
 
 build_device() {
     brunch ${1}
-    mv $OUT/lineage-*.zip $BUILD_OUTPUT/lineage-19.0-$BUILD_DATE-UNOFFICIAL-${1}$($PERSONAL && echo "-personal" || echo "").zip
+    mv $OUT/lineage-*.zip $BUILD_OUTPUT/lineage-19.1-$BUILD_DATE-UNOFFICIAL-${1}$($PERSONAL && echo "-personal" || echo "").zip
 }
 
 build_treble() {
     case "${1}" in
-        ("A64B") TARGET=treble_a64_bvS;;
-        ("A64BG") TARGET=treble_a64_bgS;;
-        ("64B") TARGET=treble_arm64_bvS;;
-        ("64BG") TARGET=treble_arm64_bgS;;
+        ("A64B") TARGET=a64_bvS;;
+        ("A64BG") TARGET=a64_bgS;;
+        ("64B") TARGET=arm64_bvS;;
+        ("64BG") TARGET=arm64_bgS;;
         ("bv9500plus") TARGET=bv9500plus;;
         (*) echo "Invalid target - exiting"; exit 1;;
     esac
-    IMAGE_NAME=lineage-19.0-$BUILD_DATE-UNOFFICIAL-${TARGET}$(${PERSONAL} && echo "-personal" || echo "")
+    IMAGE_NAME=lineage-19.1-$BUILD_DATE-UNOFFICIAL-${TARGET}$(${PERSONAL} && echo "-personal" || echo "")
     lunch lineage_${TARGET}-userdebug
     make -j$(nproc --all) installclean
     make -j$(nproc --all) systemimage
@@ -260,8 +183,6 @@ else
     apply_patches patches_${MODE}
     apply_patches_personal bv9500plus/patches
     apply_patches_personal bv9500plus/patches_andy_yan
-    gms_patches
-    overlay_patches
     if ${PERSONAL}
     then
         apply_patches patches_platform_personal
@@ -282,6 +203,12 @@ do
 done
 ls $BUILD_OUTPUT | grep 'lineage' || true
 du -hs $BUILD_OUTPUT/lineage*
+
+if [ ${MODE} == "treble" ]
+then
+    echo "OTA timestamp: $START"
+    echo ""
+fi
 
 END=`date +%s`
 ELAPSEDM=$(($(($END-$START))/60))
--- check-patches.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-check_patches_personal() {
-    echo "Applying patch personal ${1}"
-    bash $PWD/../treble_experimentations/check-patches.sh ./lineage_build_unified/${1}
-}
-
-check_patches_personal bv9500plus/patches
-
